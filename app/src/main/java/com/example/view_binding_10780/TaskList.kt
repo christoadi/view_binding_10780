af0b274163793bea0cd32f9a52671f69844c2081
christoadi <chrizto.adi456@gmail.com>
2022-09-15 14:25:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10780/TaskList.kt
@@ -0,0 +1,26 @@
+package com.example.view_binding_10780
+
+object TaskList{
+    val taskList = listOf<Task>(
+        Task(
+            "Chapter 1", "Sarapan",
+            "Selalu sarapan di burjo agar dompet tetap senyum"
+        ),
+        Task(
+            "Chapter 2", "Rebahan",
+            "Rebahan dan Males-malesan agar otot-otot tetap senyum"
+        ),
+        Task(
+            "Chapter 3", "Makan Siang",
+            "Makan siang di warmindo lagi-lagi agar dompet tetap senyum"
+        ),
+        Task(
+            "Chapter 4", "Tidur Siang",
+            "Tidur di siang hari untuk menghadapi kerasnya dunia malam"
+        ),
+        Task(
+            "Chapter 5", "Makan malam",
+            "Ternyata kerasnya malam hari hanya sebatas makan malam di angkringan"
+        )
+    )
+}
