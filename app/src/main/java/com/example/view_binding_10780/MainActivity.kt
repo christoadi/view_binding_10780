9085301797e92ccc4493d5cd1596989e12464696
christoadi <chrizto.adi456@gmail.com>
2022-09-15 14:35:30 +0700

    viewBinding

--- app/src/main/java/com/example/view_binding_10780/MainActivity.kt
+++ app/src/main/java/com/example/view_binding_10780/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding?.root)
         val adapter = MainAdapter(TaskList.taskList)
         binding?.taskRv?.adapter = adapter
+
     }
 
     override fun onDestroy() {
